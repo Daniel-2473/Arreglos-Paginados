066a507245634e5ee82d019e70ae9d4939135684
Daniel-2473 <daniel.ulate937@gmail.com>
2026-03-07 18:21:03 -0600

    Creación de la base de la clase PagedArray
    Se creó la clase PagedArray, con los atributos básicos necesarios

---
+++ sorter/PagedArray.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cloudzzz on 3/7/26.
+//
+
+#include "PagedArray.h"
---
+++ sorter/PagedArray.h
@@ -0,0 +1,13 @@
+//
+// Created by cloudzzz on 3/7/26.
+//
+
+#ifndef SORTER_PAGEDARRAY_H
+#define SORTER_PAGEDARRAY_H
+
+
+class PagedArray {
+};
+
+
+#endif //SORTER_PAGEDARRAY_H
