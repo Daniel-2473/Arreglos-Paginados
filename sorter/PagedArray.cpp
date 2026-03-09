64ff4197f9d3fd9ab540bf90bbd3d9ef7aea8272
Daniel-2473 <daniel.ulate937@gmail.com>
2026-03-08 21:19:36 -0600

    Implementación del método PageHit()
    El método se encarga de retornar la referencia al valor correspondiente del índice solicitido.

--- sorter/PagedArray.cpp
+++ sorter/PagedArray.cpp
@@ -1,5 +1,38 @@
-//
-// Created by cloudzzz on 3/7/26.
-//
-
 #include "PagedArray.h"
+#include <fstream>
+
+PagedArray::PagedArray(int pageSize, int pageCount, string outputFilePath, string inputFilePath) {
+    this->inputFilePath = inputFilePath;
+    this->outputFilePath = outputFilePath;
+    this->pageSize = pageSize;
+    this->pageCount = pageCount;
+    this->pages = new int*[this->pageCount];
+    for (int i = 0; i < this->pageCount; i++) {
+        this->pages[i] = new int[this->pageSize];
+    }
+    usedPages = new int[this->pageCount];
+    for (int i = 0; i<this->pageCount; i++) {
+        *(usedPages + i) = -1;
+    }
+}
+int& PagedArray::operator[](int index) {
+    int page = index/pageSize;
+    int pageIndex = index % pageSize;
+    int frame = -1;
+    bool pageHit = false;
+    for (int i=0; i < pageCount; i++) {
+        if (page == usedPages[i]) {
+            pageHit = true;
+            frame = i;
+            break;
+        }
+    }
+    if (pageHit == true) {
+        return PageHit(frame, pageIndex);
+    }
+}
+
+int& PagedArray::PageHit(int frame, int pageIndex) {
+    return pages[frame][pageIndex];
+}
+
--- sorter/PagedArray.h
+++ sorter/PagedArray.h
@@ -5,9 +5,24 @@
 #ifndef SORTER_PAGEDARRAY_H
 #define SORTER_PAGEDARRAY_H
 
+#include <iostream>
+using namespace std;
 
 class PagedArray {
-};
+private:
+    int** pages;
+    int pageCount;
+    int pageSize;
+    string outputFilePath;
+    string inputFilePath;
+    int* usedPages;
 
+public:
+    PagedArray(int pageSize, int pageCount, string outputFilePath, string inputFilePath);
+    int& operator[](int index);
+    int& PageHit(int frame, int pageIndex);
+    void PageFault();
+
+};
 
 #endif //SORTER_PAGEDARRAY_H
